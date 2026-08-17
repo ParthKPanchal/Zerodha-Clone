96c3885938044404d87fb4cba9438934d94257e7
Parth Panchal <panchalparth93@yahoo.in>
2026-08-17 13:47:37 +0530

    Support Page

--- frontend/src/pages/support/CreateTicket.jsx
+++ frontend/src/pages/support/CreateTicket.jsx
@@ -1,3 +1,189 @@
 export default function CreateTicket() {
-  return <h2>CreateTicket</h2>;
+  return (
+    <section className="container py-5">
+      <div className="row">
+        <div className="col-12 mb-4">
+          <h1 className="fs-2">To Create a ticket, select a relevant topic</h1>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4 mb-5">
+          <h4 className="fs-5 mb-3">
+            <i className="fa-solid fa-circle-plus me-2"></i>
+            Account Opening
+          </h4>
+
+          <div className="lh-lg">
+            <a href="#" className="text-decoration-none d-block">
+              Resident individual
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Minor
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Non Resident Indian (NRI)
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Company, Partnership, HUF and LLP
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Glossary
+            </a>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4 mb-5">
+          <h4 className="fs-5 mb-3">
+            <i className="fa-solid fa-circle-plus me-2"></i>
+            Your Zerodha Account
+          </h4>
+
+          <div className="lh-lg">
+            <a href="#" className="text-decoration-none d-block">
+              Your Profile
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Account modification
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Client Master Report (CMR) and Depository Participant (DP)
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Nomination
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Transfer and conversion of securities
+            </a>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4 mb-5">
+          <h4 className="fs-5 mb-3">
+            <i className="fa-solid fa-circle-plus me-2"></i>
+            Kite
+          </h4>
+
+          <div className="lh-lg">
+            <a href="#" className="text-decoration-none d-block">
+              IPO
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Trading FAQs
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Margin Trading Facility (MTF) and Margins
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Charts and orders
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Alerts and Nudges
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              General
+            </a>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4 mb-5">
+          <h4 className="fs-5 mb-3">
+            <i className="fa-solid fa-circle-plus me-2"></i>
+            Funds
+          </h4>
+
+          <div className="lh-lg">
+            <a href="#" className="text-decoration-none d-block">
+              Add money
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Withdraw money
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Add bank accounts
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              eMandates
+            </a>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 col-lg-4 mb-5">
+          <h4 className="fs-5 mb-3">
+            <i className="fa-solid fa-circle-plus me-2"></i>
+            Console
+          </h4>
+
+          <div className="lh-lg">
+            <a href="#" className="text-decoration-none d-block">
+              Portfolio
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Corporate actions
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Funds statement
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Reports
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Profile
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Segments
+            </a>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 col-lg-4 mb-5">
+          <h4 className="fs-5 mb-3">
+            <i className="fa-solid fa-circle-plus me-2"></i>
+            Coin
+          </h4>
+
+          <div className="lh-lg">
+            <a href="#" className="text-decoration-none d-block">
+              Mutual funds
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              National Pension Scheme (NPS)
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Fixed Deposit (FD)
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Features on Coin
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              Payments and Orders
+            </a>
+
+            <a href="#" className="text-decoration-none d-block">
+              General
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- frontend/src/pages/support/Hero.jsx
+++ frontend/src/pages/support/Hero.jsx
@@ -1,3 +1,64 @@
 export default function Hero() {
-  return <h2>Hero</h2>;
+  return (
+    <section className="container-fluid bg-primary text-light">
+      <div className="container py-4">
+        <div className="d-flex justify-content-between align-items-center">
+          <h3 className="mb-0">Support Portal</h3>
+
+          <a href="" className="text-light text-decoration-none">
+            Track Tickets
+          </a>
+        </div>
+      </div>
+      <div className="container py-5">
+        <div className="row align-items-center g-5">
+          <div className="col-12 col-lg-7">
+            <h1 className="mb-4">
+              Search for an answer or browse help topics to create a ticket
+            </h1>
+
+            <input
+              type="text"
+              placeholder="Eg. how do I activate F&O"
+              className="form-control form-control-lg mb-4"
+            />
+            <div className="d-flex flex-wrap gap-3">
+              <a href="" className="text-light">
+                Track account opening
+              </a>
+
+              <a href="" className="text-light">
+                Track segment activation
+              </a>
+
+              <a href="" className="text-light">
+                Intraday margins
+              </a>
+
+              <a href="" className="text-light">
+                Kite user manual
+              </a>
+            </div>
+          </div>
+          <div className="col-12 col-lg-5">
+            <h2 className="mb-4">Featured</h2>
+
+            <ol className="lh-lg">
+              <li>
+                <a href="" className="text-light">
+                  Current Takeovers and Delisting - January 2024
+                </a>
+              </li>
+
+              <li>
+                <a href="" className="text-light">
+                  Latest Intraday leverages - MIS & CO
+                </a>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
