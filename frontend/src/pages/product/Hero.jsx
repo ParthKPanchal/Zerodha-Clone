fc6f4af402dd2e5c76b699cd14acfc189c246a35
Parth Panchal <panchalparth93@yahoo.in>
2026-08-17 11:45:36 +0530

    Product Page

--- frontend/src/pages/product/Hero.jsx
+++ frontend/src/pages/product/Hero.jsx
@@ -1,3 +1,18 @@
 export default function Hero() {
-  return <h2>Hero</h2>;
+  return (
+    <section className="container p-5 border-bottom mb-5">
+      <div className="row text-center mt-5">
+        <h2>Zerodha Products</h2>
+        <h3 className="text-muted mt-1">
+          Sleek, modern, and intuitive trading platforms
+        </h3>
+        <p className="mt-2">
+          Check out our{" "}
+          <a href="" className="text-decoration-none">
+            investment offerings <i class="fa-solid fa-right-long"></i>
+          </a>
+        </p>
+      </div>
+    </section>
+  );
 }
